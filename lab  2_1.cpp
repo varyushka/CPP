9ea0846c673b4f1d48fb74c442becdf8e61291e8
varyushka <120749878+varyushka@users.noreply.github.com>
2023-09-19 21:06:43 +0300

    lab  2_1.cpp

--- lab  2_1.cpp
+++ lab  2_1.cpp
@@ -9,7 +9,6 @@ int main()
     int a;
     int c;
     double result;
-
     cout << "Введіть значення x ";
     cin >> x;
     cout << "Введіть значення a ";
@@ -18,12 +17,10 @@ int main()
     cin >> b;
     cout << "Введіть значення c ";
     cin >> c;
-
     if (x >= 1.5)
     {
         result = ((pow(x, 3) + (3 * a)) * c) / (a - b + x);
     }
-
     if (x < 1.5)
     {
         result = pow(sin(a), 2) * pow(sin(b), 2);
